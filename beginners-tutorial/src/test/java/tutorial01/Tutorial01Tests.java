fde57dad8c512254d2c613d59c157c55a96f8e4e
Johannes Link <business@johanneslink.net>
2024-04-23 10:21:56 +0200

    Added new module "beginners-tutorial" for djl tutorials

---
+++ beginners-tutorial/src/test/java/tutorial01/Tutorial01Tests.java
@@ -0,0 +1,20 @@
+package tutorial01;
+
+import org.junit.jupiter.api.*;
+
+import ai.djl.*;
+import ai.djl.nn.*;
+import ai.djl.nn.core.*;
+import ai.djl.training.*;
+
+/**
+ * See http://docs.djl.ai/docs/demos/jupyter/tutorial/01_create_your_first_network.html
+ */
+class Tutorial01Tests {
+
+	@Test
+	void runTutorial() {
+		Application application = Application.CV.IMAGE_CLASSIFICATION;
+	}
+
+}
