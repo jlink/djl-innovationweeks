65dcc408978de1c4be73d5e9923237638a87c74c
Johannes Link <business@johanneslink.net>
2024-04-25 13:56:52 +0200

    Added resources.txt for Java in ML stuff

--- beginners-tutorial/src/test/java/tutorial01/Tutorial01Tests.java
+++ beginners-tutorial/src/test/java/tutorial01/Tutorial01Tests.java
@@ -15,6 +15,18 @@ class Tutorial01Tests {
 	@Test
 	void runTutorial() {
 		Application application = Application.CV.IMAGE_CLASSIFICATION;
+		long inputSize = 28*28;
+		long outputSize = 10;
+		SequentialBlock block = new SequentialBlock();
+		block.add(Blocks.batchFlattenBlock(inputSize));
+		block.add(Linear.builder().setUnits(128).build());
+		block.add(Activation::relu);
+		block.add(Linear.builder().setUnits(64).build());
+		block.add(Activation::relu);
+		block.add(Linear.builder().setUnits(outputSize).build());
+
+		System.out.println(block);
+
 	}
 
 }
